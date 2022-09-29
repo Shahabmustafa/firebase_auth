777446090707dc8b1f7115396be697ed60c72cb3
Shahab Mustafa <you@example.com>
2022-09-30 03:16:30 +0500

    error firebase

---
+++ bilal_mustafa/android/app/src/main/kotlin/com/example/bilal_mustafa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bilal_mustafa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bilal_mustafa/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:bilal_mustafa/home_page.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
